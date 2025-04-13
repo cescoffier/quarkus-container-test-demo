010115f04deebb9e22f8b8e8e0837d2f63b62302
Clement Escoffier <clement.escoffier@gmail.com>
2025-04-13 20:50:08 +0200

    Add trace

--- src/test/java/me/escoffier/MoviesEndpointIT.java
+++ src/test/java/me/escoffier/MoviesEndpointIT.java
@@ -2,6 +2,7 @@ package me.escoffier;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 
 @QuarkusIntegrationTest
 class MoviesEndpointIT extends MoviesEndpointTest {
@@ -9,7 +10,9 @@ class MoviesEndpointIT extends MoviesEndpointTest {
 
 
     @Override
+    @BeforeEach
     public void setup() {
+        System.out.println(">>>>>>>>> " + RestAssured.baseURI);
         RestAssured.delete("/movies");
     }
 }
